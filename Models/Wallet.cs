6ad2e9bfa00c7072f37e039cbbc67428e12b8f20
SANDILE BRAMWELL Munyadziwa <146029292+Immortal8725@users.noreply.github.com>
2026-07-21 12:34:22 +0200

    Delete Models directory

--- Models/Wallet.cs
+++
@@ -1,9 +0,0 @@
-namespace DigitalBusSystem.Models
-{
-    public class Wallet
-    {
-        public int Id { get; set; }
-        public int UserId { get; set; }
-        public decimal Balance { get; set; } = 0; // Step 2: Top-Up
-    }
-}
